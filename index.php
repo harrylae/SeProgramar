4211db202a4108555540bb027ef7bd940a62817f
Lucas Escribano <harry_lae@hotmail.com>
2022-03-17 16:45:32 -0300

    Agregado el Bootstrap y cotejando que funcione.

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+
   <head>
-    <meta charset=UTF-8>
-    <title>PÁGINA EN CONSTRUCCIÓN</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Instalación atada, compilada y minimizada</title>
+
   </head>
+
   <body>
-      
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin= "anonymous"></script>
+
+    <div class="container-fluid p-3 my-3 bg-primary text-white">
+      <h1>Prueba de Página</h1>
+      <p>Este es algún texto para mostrar.</p>
+    </div> 
+
   </body>
+
 </html>
